a68b2053ba7598e3069100c5e7ffb7171d8ea065
Joshua Spears <joshuaspears@FBI-Surveillance-Team-6.local>
2017-10-12 15:26:47 -0700

    Built main game

---
+++ assets/javascript/game.js
@@ -0,0 +1,179 @@
+$(document).ready(function() {
+
+// ======================== VARIABLES ==========================
+
+// CREATE VARIABLES FOR 
+	var magicNumber;
+		
+	var wins = 0; 
+	var losses = 0; 
+	
+	var score;
+
+	var jewelOne;
+	var jewelTwo;
+	var jewelThree;
+	var jewelFour;
+
+// ======================== FUNCTIONS ========================== 
+	
+// FUNCTION GENMAGICNUMBER
+function genMagicNumber() {
+	// ASSIGN MAGIC NUMBER A RANDOMLY SELECTED NUMBER FROM 19-120
+	magicNumber = Math.floor((Math.random() * 101) + 19);
+		// .TEXT MAGIC NUMBER
+		$("#magic-number").text(magicNumber);
+};
+
+// FUNCTION SETWINLOSS
+function setWinLoss() {
+	// .TEXT WINS 
+	$("#wins").text(wins);
+	// .TEXT LOSSES
+	$("#losses").text(losses);	
+};
+
+// FUNCTION RESETSCORE
+function resetScore() {
+	// MAKE VARIABLE SCORE = 0
+	score = 0;
+		// .TEXT SCORE
+		$("#score").text(score); 
+};
+
+// FUNCTION GENJEWELVALUE
+function genJewelValue() {
+	// ASSIGN EACH JEWEL A RANDOMLY GENERATED NUMBER BETWEEN 1 & 9
+	jewelOne = Math.floor((Math.random() * 6) + 1);
+	jewelTwo = Math.floor((Math.random() * 6) + 1);
+	jewelThree = Math.floor((Math.random() * 6) + 1);
+	jewelFour = Math.floor((Math.random() * 6) + 1);
+		// CHECK VARIABLES
+		console.log(jewelOne, jewelTwo, jewelThree, jewelFour);
+};
+
+// FUNCTION WIN
+function win() {
+	// ALERT YOU WON
+	alert("You've Won!");
+	// ADD 1 TO WINS
+	wins++;
+		// .TEXT WINS++
+		$("#wins").text(wins);
+			// CALL GENMAGICNUMBER
+			genMagicNumber();
+				// CALL RESETSCORE
+				resetScore();
+					// CALL GENJEWELVALUE
+					genJewelValue();
+};
+
+// FUNCTION LOSS
+function loss() {
+	// ALERT YOU LOST
+	alert("You've Lost!");  
+		// ADD 1 TO LOSSES
+		losses++;
+			// .TEXT LOSSES++
+			$("#losses").text(losses);
+				// CALL GENMAGICNUMBER
+				genMagicNumber();
+					// CALL RESETSCORE
+					resetScore();
+						// CALL GENJEWELVALUE
+						genJewelValue();
+};
+
+
+// CALL RESETSCORE
+resetScore();
+// CALL GENMAGICNUMBER
+genMagicNumber();
+// CALL SETWINLOSS
+setWinLoss();
+// CALL GENJEWELVALUE
+genJewelValue();
+
+// ======================== ONCLICKS ===========================
+
+// ONCLICK JEWEL1
+$("#jewel1").on("click", function() {
+	// SCORE += JEWELONE
+	score += jewelOne;
+		// SCORE .TEXT SCORE 
+		$("#score").text(score);
+				// IF SCORE IS === MAGICNUMBER
+				if(score === magicNumber) {
+					// CALL WIN FUNCTION
+					win();
+					// ELSE IF SCORE > MAGICNUMBER
+				}else if(score > magicNumber) {
+					// CALL LOSS FUNCTION
+					loss();
+				};
+
+});// END ONCLICK JEWEL1 
+
+// ONCLICK JEWEL2
+$("#jewel2").on("click", function() {
+	// SCORE += JEWELONE
+	score += jewelTwo;
+		// SCORE .TEXT SCORE 
+		$("#score").text(score);
+				// IF SCORE IS === MAGICNUMBER
+				if(score === magicNumber) {
+					// CALL WIN FUNCTION
+					win();
+					// ELSE IF SCORE > MAGICNUMBER
+				}else if(score > magicNumber) {
+					// CALL LOSS FUNCTION
+					loss();
+				};
+
+});// END ONCLICK JEWEL2
+
+// ONCLICK JEWEL3
+$("#jewel3").on("click", function() {
+	// SCORE += JEWELONE
+	score += jewelThree;
+		// SCORE .TEXT SCORE 
+		$("#score").text(score);
+				// IF SCORE IS === MAGICNUMBER
+				if(score === magicNumber) {
+					// CALL WIN FUNCTION
+					win();
+					// ELSE IF SCORE > MAGICNUMBER
+				}else if(score > magicNumber) {
+					// CALL LOSS FUNCTION
+					loss();
+				};
+
+});// END ONCLICK JEWEL3
+
+// ONCLICK JEWEL4
+$("#jewel4").on("click", function() {
+	// SCORE += JEWELONE
+	score += jewelFour;
+		// SCORE .TEXT SCORE 
+		$("#score").text(score);
+				// IF SCORE IS === MAGICNUMBER
+				if(score === magicNumber) {
+					// CALL WIN FUNCTION
+					win();
+					// ELSE IF SCORE > MAGICNUMBER
+				}else if(score > magicNumber) {
+					// CALL LOSS FUNCTION
+					loss();
+				};
+
+});// END ONCLICK JEWEL4  
+
+});// END THE READY START FUNCITON
+
+
+
+
+
+
+
+
